fc3fefd098fb8036a13d293398d1ec70e8e5e34c
Bogdan Leonov <thereisnobeam@gmail.com>
2026-05-11 20:12:54 +0300

    add incremental logic to mart_asset_prices_daily

--- dbt/models/marts/mart_asset_prices_daily.sql
+++ dbt/models/marts/mart_asset_prices_daily.sql
@@ -1,3 +1,20 @@
+{{ config(
+    materialized='incremental',
+    unique_key=['exchange', 'symbol', 'price_date']
+) }}
+
+with source as (
+    select *
+    from {{ ref('stg_asset_prices') }}
+
+    {% if is_incremental() %}
+        where date_trunc('day', fetched_at) >= (
+            select coalesce(max(price_date), '1900-01-01'::timestamp)
+            from {{ this }}
+        )
+    {% endif %}
+)
+
 select
     exchange,
     symbol,
@@ -8,7 +25,7 @@ select
     count(*) as records_count,
     min(fetched_at) as first_fetched_at,
     max(fetched_at) as last_fetched_at
-from {{ ref('stg_asset_prices') }}
+from source
 group by
     exchange,
     symbol,
